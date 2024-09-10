f00b0e40c3839f64b0485bd26c5a002a313cedcf
rossrobino <rbrobino@gmail.com>
2024-09-09 20:15:04 -0400

    test

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
 import { createApp } from "./dist/server/app.js";
 import { serveStatic } from "hono/deno";
-import { createMiddleware } from "hono/factory";
+import type { MiddlewareHandler } from "hono";
 
-const immutable = createMiddleware(async (c, next) => {
+const immutable: MiddlewareHandler = async (c, next) => {
 	await next();
-	if (c.req.path.startsWith("/_immutable")) {
-		c.header("Cache-Control", "public, immutable, max-age=31536000");
-	}
-});
+	c.header("Cache-Control", "public, immutable, max-age=31536000");
+};
 
 const app = createApp({
-	middleware: [immutable, serveStatic({ root: "./dist/client" })],
+	middleware: [
+		{
+			path: "/_immutable/*",
+			handler: immutable,
+		},
+		{
+			path: "/*",
+			handler: serveStatic({
+				root: "./dist/client",
+			}),
+		},
+	],
 });
 
 Deno.serve(app.fetch);
