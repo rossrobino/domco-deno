b35ed6f8aa18bed8327f80c9ccb83fe04c5a8c32
rossrobino <rbrobino@gmail.com>
2024-10-09 15:09:07 -0400

    test

--- src/server/+func.ts
+++ src/server/+func.ts
@@ -1,15 +1,23 @@
 import type { Handler } from "domco";
-import { html } from "client:page";
+
+// both of these types are added with the triple slash directive in src/env.d.ts
+import { html } from "client:page"; // domco/env
+import rawText from "../client/style.css?raw"; // vite/client
 
 export const handler: Handler = (req) => {
 	const { pathname } = new URL(req.url);
 
+	console.log(rawText); // works
+
 	if (pathname === "/") {
-		return new Response(html, {
-			headers: {
-				"Content-Type": "text/html",
+		return new Response(
+			html, // works
+			{
+				headers: {
+					"Content-Type": "text/html",
+				},
 			},
-		});
+		);
 	}
 
 	return new Response("Not found", { status: 404 });
